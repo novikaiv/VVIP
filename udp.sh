68e4f7cdddc117e4b9e10c22d9511059281ad8ec
dit1304 <43095037+dit1304@users.noreply.github.com>
2024-07-16 22:52:43 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -10,10 +10,10 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install udp-custom
 echo downloading udp-custom
-wget -q -O /root/udp/udp-custom "https://raw.githubusercontent.com/dit1304/main/udp/udp-custom-linux-amd64" && chmod +x /root/udp/udp-custom
+wget -q -O /root/udp/udp-custom "https://raw.githubusercontent.com/dit1304/udp/main/udp-custom-linux-amd64" && chmod +x /root/udp/udp-custom
 
 echo downloading default config
-wget -q -O /root/udp/config.json "https://raw.githubusercontent.com/dit1304/main/udp/config.json" && chmod 644 /root/udp/config.json
+wget -q -O /root/udp/config.json "https://raw.githubusercontent.com/dit1304/udp/main/config.json" && chmod 644 /root/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
