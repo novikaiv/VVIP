e3b619a27ec384f2509705379345bbcf382b10d7
dit1304 <43095037+dit1304@users.noreply.github.com>
2024-07-27 05:36:43 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-DOMAIN=.zeroostore.net
+DOMAIN=.dits-store.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.zeroostore.net
+dns=${sub}.dits-store.my.id
 CF_ID=didit.pamuji@gmail.com
 CF_KEY=fe7fd9817a73cc64c4422233a8a5d6fd7ca9c
 set -euo pipefail
